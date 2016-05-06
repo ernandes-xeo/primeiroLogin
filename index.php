0f392f553044486fa6d39d6dd5f1a8aee7a19b94
Ernandes Xeo <Ernandes Xeo>
2016-05-06 02:50:19 -0300

    update index login

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
         <link rel="stylesheet" type="text/css" href="css/layout.css" >
     </head>
     <body>
-        <h1>Cadastre-se</h1>
+        <h1>Login</h1>
         <form id='meuform' method="POST" action="validacao.php" >
-            <fieldset> <legend>Cadastro</legend>
+            <fieldset> <legend>Login</legend>
                 <label>Usuário</label>
                 <input id="nome" type="text" name="user" />
 
