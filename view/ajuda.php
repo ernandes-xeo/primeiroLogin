3e9862779cf2b8f57e79abefca014432c63eefe1
Ernandes Xeo <Ernandes Xeo>
2016-05-06 02:15:43 -0300

    telas do sistema

---
+++ view/ajuda.php
@@ -0,0 +1,20 @@
+<section id="main">
+		<div class="main-contener">
+                    <h2>Ajuda</h2>
+                        <br />
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eget sapien iaculis, molestie nunc a, vehicula lectus. Fusce et gravida enim.
+                        Nam commodo, enim a cursus lobortis, massa turpis aliquet elit, ac condimentum felis justo ut erat. Curabitur sed consectetur enim, nec fringilla leo. Phasellus ac tortor sed enim aliquet vulputate. Etiam in turpis tortor. Sed commodo blandit mauris congue pulvinar. Aenean sagittis viverra purus, id posuere erat placerat pretium. Integer feugiat tincidunt rutrum. Etiam quis tellus non orci mollis ornare. Nam tincidunt lorem congue, commodo lectus vitae, volutpat metus. Cras nisl ante, tempus eu odio eget, consequat fermentum sem. Praesent sed nibh neque.
+                        </p>
+                        <br />
+                        <p>
+                        Proin aliquam dictum turpis a placerat. Aenean commodo, libero vitae dapibus condimentum, urna eros efficitur ante, quis ornare risus mauris at eros. 
+                        Praesent viverra augue ex, vel scelerisque nibh gravida quis. Duis fermentum diam accumsan pulvinar imperdiet. Duis elementum facilisis ipsum quis pulvinar. 
+                        Sed et leo in dui ultrices malesuada. Sed aliquam est quis dolor ultricies porta. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed accumsan, 
+                        enim vitae malesuada ornare, elit enim tempus libero, vitae elementum tortor velit eu velit. Curabitur lobortis iaculis elit at pretium. In efficitur diam id 
+                        lectus lacinia pulvinar. In eu nisl scelerisque, auctor lacus nec, placerat lectus. Aliquam tempus vestibulum nisi vel pellentesque. Quisque sollicitudin posuere 
+                        vehicula. Donec sagittis nisl nunc, non venenatis quam placerat non. Ut vel erat massa.
+                        </p>
+		</div>
+		<section id='left-destaques'>Left com destaques</section>
+</section>
--- view/index.php
+++ view/index.php
@@ -26,17 +26,22 @@
             // include de aquivos para a formação da página
             include_once "header.php";
             include_once "nav.php";
-            include_once "content.php";
+            
+            
             $opcao = $_REQUEST['op'];
+            
             switch ($opcao){
             case 1:
-                
+                include_once 'servicos.php';
             break;
             case 2:
+                include_once 'clientes.php';
             break;
             case 3:
+                include_once 'produtos.php';
             break;
             case 4:
+                include_once 'ajuda.php';
             break;        
             default:
                 include_once "content.php";
