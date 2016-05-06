b27138c902bb1732850990db07f97164ac18710e
Ernandes Xeo <Ernandes Xeo>
2016-05-06 02:48:27 -0300

    update login

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
         <link rel="stylesheet" type="text/css" href="css/layout.css" >
     </head>
     <body>
-        <h1>Cadastre-se</h1>
+        <h1>Login</h1>
         <form id='meuform' method="POST" action="validacao.php" >
-            <fieldset> <legend>Cadastro</legend>
+            <fieldset> <legend>Efetue seu login</legend>
                 <label>Usuário</label>
                 <input id="nome" type="text" name="user" />
 
